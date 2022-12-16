e629d8b74ea90272781187e2f5e19a6615978b2b
Nico Hanisch <donsprallo@gmail.com>
2022-12-16 18:18:05 +0100

    Add Healthy interface for HealthcheckEndpoint

--- internal/web/api/routes/healthcheck.go
+++ internal/web/api/routes/healthcheck.go
@@ -6,19 +6,34 @@ import (
 	"net/http"
 )
 
+// Healthy Interface is used to check the health status of a system.
+type Healthy interface {
+
+	// IsHealthy checks if something is OK or faulty. When all is OK
+	// true is returned, otherwise false is returned.
+	IsHealthy() bool
+
+	// Error returns an error message that provides a reason if IsHealthy
+	// returns false.
+	Error() string
+}
+
 // HealthcheckEndpoint is used to check anything that may interrupt
 // the API from serving incoming requests. The default route "/" is
 // designed to check the service as canary. This means that a status
 // value other than the expected one indicates a serious error. Other
 // routes should be used for further status checks.
 type HealthcheckEndpoint struct {
-	handler http.Handler
+	handler  http.Handler       // The http handler
+	checkers map[string]Healthy // A map of health checkers
 }
 
 // NewHealthcheckEndpoint creates a new api.Endpoint for healthcheck
 // capabilities. The endpoint must be registered with a http.server.
 func NewHealthcheckEndpoint() api.Endpoint {
-	return &HealthcheckEndpoint{}
+	return &HealthcheckEndpoint{
+		checkers: make(map[string]Healthy),
+	}
 }
 
 // RegisterRoutes implements api.Endpoint interface.
@@ -32,18 +47,32 @@ func (e *HealthcheckEndpoint) RegisterRoutes(router *mux.Router) {
 		Methods(http.MethodGet)
 }
 
+// AddChecker adds a Healthy checkers with a name to the HealthcheckEndpoint.
+// The checkers is used in a healthcheck request to check the state of the
+// system.
+func (e *HealthcheckEndpoint) AddChecker(
+	name string, checker Healthy) {
+	e.checkers[name] = checker
+}
+
+// RemoveChecker deletes a Healthy checkers from the HealthcheckEndpoint.
+func (e *HealthcheckEndpoint) RemoveChecker(name string) {
+	delete(e.checkers, name)
+}
+
 // HealthcheckResponse is the response type for the HealthcheckEndpoint
-// default route. The response contains only a boolean to display the
-// API status.
+// healthcheck route. The response contains a boolean to display the API
+// status and a map of errors.
 type HealthcheckResponse struct {
-	Status bool `json:"status"`
+	Status bool              `json:"status"`
+	Errors map[string]string `json:"errors"`
 }
 
 // PingResponse is the response type for the HealthcheckEndpoint ping
-// route. The response contains only a boolean to display that the API
+// route. The response contains only a string to display that the API
 // is available.
 type PingResponse struct {
-	Status bool `json:"status"`
+	Status string `json:"status"`
 }
 
 // The healthcheck route of the HealthcheckEndpoint verifies multiple items
@@ -52,16 +81,25 @@ type PingResponse struct {
 func (e *HealthcheckEndpoint) healthcheck(
 	w http.ResponseWriter, _ *http.Request,
 ) {
+	// Check all dependencies. On error add information to map.
+	apiErrors := make(map[string]string)
+	for name, checker := range e.checkers {
+		if !checker.IsHealthy() {
+			// Add info on error detection.
+			apiErrors[name] = checker.Error()
+		}
+	}
 	// Disable cache to prevent http caching from serving the request.
 	w.Header().Add("Cache-Control", "no-cache")
 	api.MustJsonResponse(w, HealthcheckResponse{
-		Status: true,
+		Status: len(apiErrors) == 0,
+		Errors: apiErrors,
 	}, http.StatusOK)
 }
 
 // The ping route of the HealthcheckEndpoint barely checks that the API is
 // running and the service is accessible. For this the endpoint always return
-// the same result where status is true. Any other response indicates a
+// the same result where status is "running". Any other response indicates a
 // critical system failure.
 func (e *HealthcheckEndpoint) ping(
 	w http.ResponseWriter, _ *http.Request,
@@ -69,6 +107,6 @@ func (e *HealthcheckEndpoint) ping(
 	// Disable cache to prevent http caching from serving the request.
 	w.Header().Add("Cache-Control", "no-cache")
 	api.MustJsonResponse(w, PingResponse{
-		Status: true,
+		Status: "running",
 	}, http.StatusOK)
 }
