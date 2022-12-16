fa589d7f3068f87ca2eeeb2a063fcbdf98a01ef8
Nico Hanisch <donsprallo@gmail.com>
2022-12-16 17:33:59 +0100

    Documentation of healthcheck api endpoint

--- internal/web/api/routes/healthcheck.go
+++ internal/web/api/routes/healthcheck.go
@@ -6,14 +6,22 @@ import (
 	"net/http"
 )
 
+// HealthcheckEndpoint is used to check anything that may interrupt
+// the API from serving incoming requests. The default route "/" is
+// designed to check the service as canary. This means that a status
+// value other than the expected one indicates a serious error. Other
+// routes should be used for further status checks.
 type HealthcheckEndpoint struct {
 	handler http.Handler
 }
 
+// NewHealthcheckEndpoint creates a new api.Endpoint for healthcheck
+// capabilities. The endpoint must be registered with a http.server.
 func NewHealthcheckEndpoint() api.Endpoint {
 	return &HealthcheckEndpoint{}
 }
 
+// RegisterRoutes implements api.Endpoint interface.
 func (e *HealthcheckEndpoint) RegisterRoutes(router *mux.Router) {
 	e.handler = router
 
@@ -22,12 +30,16 @@ func (e *HealthcheckEndpoint) RegisterRoutes(router *mux.Router) {
 		Methods(http.MethodGet)
 }
 
+// HealthcheckResponse is the response type for the HealthcheckEndpoint
+// default route. The response contains only a boolean to display the
+// API status.
 type HealthcheckResponse struct {
 	Status bool `json:"status"`
 }
 
-// Get the healthcheck response. The healthcheck always return the same
-// result. This enables an easy way to automatically check the result.
+// This is the default route for the HealthcheckEndpoint. The healthcheck
+// route always response with the HealthcheckResponse and status true. Any
+// other response indicates a critical system failure.
 func (e *HealthcheckEndpoint) healthcheck(
 	w http.ResponseWriter, _ *http.Request,
 ) {
